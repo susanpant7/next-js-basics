7bcec94e7a22025caaf929a97e39b12ee86e2d5f
susanpant7 <susan.pant7@gmail.com>
2025-12-04 15:20:31 -0500

    adding image

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+
 export default function About() {
   return (
-    <div>About</div>
+    <>
+        <div>About</div>
+        {/* this is a static image */}
+        {/* next js automatically optimizes the image */}
+        {/* priority is used to tell next js that this image is important */}
+        {/* width and height are used to tell next js the size of the image */}
+        {/* alt is used to describe the image for accessibility */}
+        {/* src is the path to the image. It is relative to the public folder */}
+        <Image
+          className="dark:invert"
+          src="/globe.svg"
+          alt="about page image"
+          width={100}
+          height={20}
+          priority
+        />
+    </>
   )
 }
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+
 export default function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <>
+      <div>Contact</div>
+      {/* this is a dynamic image */}
+      {/* the hostname is the domain name of the website
+        the hostname needs to be added to the next.config.js file */}
+      <Image
+        className="dark:invert"
+        src="https://m.media-amazon.com/images/G/01/DiscoTec/2024/CategoryFlips/2025/Spring_Summer/CE/EN/Summer/Browse/CE_Summer25_4594-DT-and-MOB-432x432-EN._CB550344832_UC290,290_.jpg"
+        alt="Next.js logo"
+        width={100}
+        height={20}
+        priority
+      />
+    </>
+  );
 }
