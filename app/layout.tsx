352de2f379105564c3aadfa4fb728ecba1fa1870
susanpant7 <susan.pant7@gmail.com>
2025-12-04 15:01:26 -0500

    created simple navbar links

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {/* anything here appears on every page */}
+        <Link href="/"> Home | </Link>
+        <Link href="/about">About | </Link>
+        <Link href="/contact">Contact</Link>
         {children} {/* this is where the page content goes */}
       </body>
     </html>
